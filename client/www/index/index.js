03d9b50626c77bc7a57830baf0b52162b2244076
Calvin Ku <cjwku@stu.ust.hk>
2018-01-13 22:59:49 +0800

    iron router implemented with index.js and index.html

---
+++ client/www/index/index.js
@@ -0,0 +1,19 @@
+import { Template } from 'meteor/templating'
+
+Router.route('/', function () {
+	this.render('index');
+}, {
+	data: {}
+});
+
+Template.index.onCreated(function () {
+
+});
+
+Template.index.onRendered(function () {
+
+});
+
+Template.index.events({});
+
+Template.index.helpers({});
