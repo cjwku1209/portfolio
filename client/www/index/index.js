b55c2ce11c49ff83b0793bf8836abc6867940313
Calvin Ku <cjwku@stu.ust.hk>
2018-01-15 22:32:17 +0800

    UI update

--- client/www/index/index.js
+++ client/www/index/index.js
@@ -7,13 +7,16 @@ Router.route('/', function () {
 });
 
 Template.index.onCreated(function () {
-
 });
 
 Template.index.onRendered(function () {
+	var scaleVal = Math.min($(window).width()/$("home").innerWidth(), $(window).height()/$("home").innerHeight());
+
 
 });
 
 Template.index.events({});
 
 Template.index.helpers({});
+
+
