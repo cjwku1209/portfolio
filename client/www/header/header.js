9ce4c5975004b016cb608bd94abda8b84ac8fdb5
Calvin Ku <cjwku@stu.ust.hk>
2018-01-24 00:01:01 +0800

    Home button and portfolio button implmented

--- client/www/header/header.js
+++ client/www/header/header.js
@@ -8,6 +8,13 @@ Template.header.onRendered(function () {
 	document.title = "My new title";
 });
 
-Template.header.events({});
+Template.header.events({
+	'click #id_header_item_home': () => {
+		window.location = '/';
+	},
+	'click #id_header_item_portfolio': () => {
+		window.location = '/portfolio';
+	},
+});
 
 Template.header.helpers({});
